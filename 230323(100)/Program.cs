4d9054150aec637dc0e5afcdd0502e235fd582c0
simgksk <Daum_c0m@naver.com>
2023-03-23 08:55:36 +0900

    초기 커밋.

---
+++ 230323(100)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _230323_100_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
